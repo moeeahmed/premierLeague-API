aa667bef1dea4a6a0cc1b2f4cb0597a9ebb9e5f1
moeeahmed <m.ahamedbusiness@gmail.com>
2022-11-16 12:16:17 +0000

    Added rate limiter to password reset API call

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const userController = require('../controller/userController');
 const authController = require('../controller/authController');
+const rateLimit = require('express-rate-limit');
+
+//middleware
 
 const router = express.Router();
 
@@ -8,7 +11,15 @@ const router = express.Router();
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 router.post('/logout', authController.logout);
-router.post('/forgotPassword', authController.forgotPassword);
+router.post(
+  '/forgotPassword',
+  rateLimit({
+    windowMs: 24 * 60 * 60 * 1000,
+    max: 5,
+    message: 'Too many password resets requested ',
+  }),
+  authController.forgotPassword
+);
 router.patch('/resetPassword', authController.resetPassword);
 
 //Routes for updating anything to do with settings
