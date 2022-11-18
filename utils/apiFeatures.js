c26f94797b5711ee1b36a9c30872d50100cc1db5
moeeahmed <m.ahamedbusiness@gmail.com>
2022-11-18 15:37:44 +0000

    Removed limit and fields code as not required

--- utils/apiFeatures.js
+++ utils/apiFeatures.js
@@ -2,13 +2,11 @@ class APIFeatures {
   constructor(query, queryString) {
     this.query = query;
     this.queryString = queryString;
-    this.page;
-    this.limit;
   }
 
   filter() {
     const queryObj = { ...this.queryString };
-    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    const excludedFields = ['page', 'sort'];
 
     excludedFields.forEach((el) => delete queryObj[el]);
 
@@ -16,6 +14,7 @@ class APIFeatures {
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
 
     this.query = this.query.find(JSON.parse(queryStr));
+    console.log(JSON.parse(queryStr));
 
     return this;
   }
@@ -23,34 +22,10 @@ class APIFeatures {
   sort() {
     if (this.queryString.sort) {
       const sortBy = this.queryString.sort.split(',').join(' ');
-
-      //localhost/?sort=player.age,player.weight
       this.query = this.query.sort(sortBy);
     } else {
-      this.query = this.query.sort('statistics.team.name');
+      this.query = this.query.sort('Date');
     }
-
-    return this;
-  }
-
-  limitField() {
-    //field limiting
-    if (this.queryString.fields) {
-      const fields = this.queryString.fields.split(',').join(' ');
-      this.query = this.query.select(fields);
-    } else {
-      this.query = this.query.select('-__v');
-    }
-
-    return this;
-  }
-
-  paginate() {
-    this.page = +this.queryString.page || 1;
-    this.limit = +this.queryString.limit || 20;
-    const skip = this.limit * (this.page - 1);
-
-    this.query = this.query.skip(skip).limit(this.limit);
 
     return this;
   }
