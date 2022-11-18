1ae0ce8353bcbf9052d7976c8856aa6800e8bd46
moeeahmed <m.ahamedbusiness@gmail.com>
2022-11-18 13:31:49 +0000

    Renamed standings route to better make sense

--- routes/fixtureRoutes.js
+++ routes/fixtureRoutes.js
@@ -1,14 +1,8 @@
 const express = require('express');
-const tableController = require('../controller/tableController');
 const fixtureController = require('../controller/fixtureController');
 const authController = require('../controller/authController');
 
 const router = express.Router();
-
-//Route to update the table
-router
-  .route('/updateTable')
-  .put(authController.protect, tableController.updateTable);
 
 //Route to update the fixtures
 router.route('/updateFixture').patch(fixtureController.updateFixture);
@@ -24,8 +18,7 @@ router.route('/getFixtures/:fixture').get(fixtureController.getFixtures);
 //Route to get a average stats for a team
 router.route('/getAverageStats/:team').get(fixtureController.getAverageStats);
 
-router
-  .route('/getTableFromFixtures')
-  .get(fixtureController.makeTableFromFixtures);
+//Get current table standing
+router.route('/tableStanding').get(fixtureController.tableStanding);
 
 module.exports = router;
