73bb90e32f3e490a7a7c5f7a92de61cfd0c42e6b
moeeahmed <m.ahamedbusiness@gmail.com>
2023-04-30 01:46:20 +0100

    Added players api route

--- models/playersModel.js
+++ models/playersModel.js
@@ -3,45 +3,56 @@ const validator = require('validator');
 
 //User schema
 const playersSchema = mongoose.Schema({
+  id: {
+    type: String,
+    required: [true, 'Please enter the players id'],
+  },
   name: {
     type: String,
-    required: [true, 'Please enter your name'],
-  },
-  email: {
-    type: String,
-    required: [true, 'Please enter your name'],
-    unique: true,
+    required: [true, 'Please enter the players name'],
     lowercase: true,
-    validate: [validator.isEmail, 'Please enter a valid email'],
   },
-  photo: { type: String, default: 'default.jpg' },
-  role: {
+  firstname: {
     type: String,
-    enum: ['user', 'admin'],
-    default: 'user',
+    required: [true, 'Please enter the players firstname'],
+    lowercase: true,
   },
-  password: {
+  lastname: {
     type: String,
-    required: [true, 'Please enter a password'],
-    minlength: 8,
-    select: false,
+    required: [true, 'Please enter the players lastname'],
+    lowercase: true,
   },
-  passwordConfirm: {
+  nationality: {
     type: String,
-    required: [true, 'Please confirm your password'],
-    validate: {
-      validator: function (el) {
-        return el === this.password; //check if both passwords are equal
-      },
-      message: 'Passwords must match',
-    },
+    required: [true, 'Please enter the players nationality'],
+    lowercase: true,
   },
-  passwordChangedAt: Date,
-  passwordResetToken: String,
-  passwordResetExpires: Date,
-  active: {
-    type: Boolean,
-    default: true,
+  league: {
+    type: String,
+    required: [true, 'Please enter the players current league'],
+    lowercase: true,
+  },
+  photo: { type: String },
+  number: {
+    type: Number,
+    required: [true, 'Please enter the players jersey number'],
+  },
+  position: {
+    type: String,
+    required: [true, 'Please enter the current position'],
+    lowercase: true,
+  },
+  team: {
+    type: String,
+    required: [true, 'Please enter the team name'],
+  },
+  teamLogo: {
+    type: String,
+    required: [true, 'Please enter the url for the team logo'],
+  },
+  birthdate: {
+    type: Date,
+    required: [true, 'Please enter the players D.o.B'],
   },
 });
 
